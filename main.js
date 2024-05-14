6a46309242ef1e1d367d831361f1a9235f074e62
Artemy Aprelikov <aprel1415@yandex.ru>
2024-05-14 17:50:02 +0300

    2nd try

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ const inputEl = document.querySelector('#search');
 inputEl.addEventListener('submit', (e) => {
     e.preventDefault();
 })
+
+const search = document.querySelector('#searchFunc');
+search.oninput= function() {
+    const value = this.value.trim();
+    const list = document.querySelectorAll('#newsSection .newsBlock')
+    if(value != ''){
+        list.forEach(elem => {
+            if(elem.innerText.search(value) == -1){
+                elem.classList.add('hide')
+            }
+        });
+    }
+    console.log(value);
+}
